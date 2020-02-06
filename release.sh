13513ac1f32e259f27e980d70a33f26cc4faa525
erdogant <erdogant@gmail.com>
2020-02-06 22:54:09 +0100

    0.1.3

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "release your packge.."
-python "C:\Users\Erdogan\Miniconda3\envs\env_IMAGE\lib\site-packages\irelease\irelease.py"
+python "C:\Users\Erdogan\Miniconda3\lib\site-packages\irelease\irelease.py"
 read -p "Press [Enter] to close"
