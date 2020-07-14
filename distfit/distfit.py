4bfbf9381eec8151864fa072300e3e8d1e591e86
Erdogan Taskesen <erdogant@gmail.com>
2020-07-14 21:58:05 +0200

    removed st.levy_l, st.levy_stable from the full list because these are slow.

--- distfit/distfit.py
+++ distfit/distfit.py
@@ -800,12 +800,13 @@ def _get_distributions(distr):
                 print('[distfit] >Error: [%s] does not exist! <skipping>' %(getdistr))
 
     elif distr=='full':
+        # st.levy_l, st.levy_stable
         out_distr = [st.alpha, st.anglit, st.arcsine, st.beta, st.betaprime, st.bradford, st.burr, st.cauchy, st.chi, st.chi2, st.cosine,
                          st.dgamma, st.dweibull, st.erlang, st.expon, st.exponnorm, st.exponweib, st.exponpow, st.f, st.fatiguelife, st.fisk,
                          st.foldcauchy, st.foldnorm, st.frechet_r, st.frechet_l, st.genlogistic, st.genpareto, st.gennorm, st.genexpon,
                          st.genextreme, st.gausshyper, st.gamma, st.gengamma, st.genhalflogistic, st.gilbrat, st.gompertz, st.gumbel_r,
                          st.gumbel_l, st.halfcauchy, st.halflogistic, st.halfnorm, st.halfgennorm, st.hypsecant, st.invgamma, st.invgauss,
-                         st.invweibull, st.johnsonsb, st.johnsonsu, st.laplace, st.levy, st.levy_l, st.levy_stable,
+                         st.invweibull, st.johnsonsb, st.johnsonsu, st.laplace, st.levy,
                          st.logistic, st.loggamma, st.loglaplace, st.lognorm, st.lomax, st.maxwell, st.mielke, st.nakagami,
                          st.norm, st.pareto, st.pearson3, st.powerlaw, st.powerlognorm, st.powernorm, st.rdist, st.reciprocal,
                          st.rayleigh, st.rice, st.recipinvgauss, st.semicircular, st.t, st.triang, st.truncexpon, st.truncnorm, st.tukeylambda,
