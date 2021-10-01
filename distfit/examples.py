d490ea2c70013f2e267fbadf00261f5b5e8ae844
erdogant <erdogant@gmail.com>
2021-10-01 11:14:48 +0200

    1.4.1

--- distfit/examples.py
+++ distfit/examples.py
@@ -319,8 +319,9 @@ X = np.random.normal(10, 3, 2000)
 y = [3,4,5,6,10,11,12,18,20]
 
 # Initialize
-dist = distfit(method='percentile', alpha=0.05, todf=False)
+# dist = distfit(method='percentile', alpha=0.05, todf=False)
 # dist = distfit(method='quantile', alpha=0.05, todf=False)
+dist = distfit(method='parametric', alpha=0.05, todf=False)
 dist.fit_transform(X)
 dist.plot()
 
