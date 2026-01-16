9377fda59dc9b080d9c3b37f6fffa3a052a20d09
erdogant <erdogant@gmail.com>
2026-01-16 15:18:32 +0100

    add missing library to req and toml

---
+++ distfit/tests/test_multidistfit.py
@@ -0,0 +1,8 @@
+import numpy as np
+from distfit import distfit
+import unittest
+from scipy.stats import binom
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')  # Use non-interactive backend for tests
+
